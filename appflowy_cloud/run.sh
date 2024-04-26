ae15c20f5793f83b4f6be56d4b339a670f62447a
Silvio Messi <messisilvio@gmail.com>
2024-04-26 23:12:40 +0200

    Increase sleep time

--- appflowy_cloud/run.sh
+++ appflowy_cloud/run.sh
@@ -3,12 +3,12 @@ su postgres -c 'pg_ctl start -D /var/lib/postgresql/data'
 su postgres -c 'psql -f /appflowy_cloud/20230312043000_supabase_auth.sql'
 redis-server &
 minio server /minio &
-sleep 2
+sleep 15
 cd /gotrue
 ./gotrue &
-sleep 2
+sleep 15
 cd /appflowy_cloud
 ./appflowy_cloud &
 ./admin_frontend &
-sleep 2
+sleep 15
 nginx -g "daemon off;"
